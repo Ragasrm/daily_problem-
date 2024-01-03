d05208edc4bf03001dd5dbb1acc4d54cafa661d9
Ragav <Srm482@gmail.com>
2024-01-03 21:02:31 +0530

    check valid palindrome

---
+++ 2.Palindrome.py
@@ -0,0 +1,22 @@
+"""
+Ex: given strings
+
+    "level", return true
+    "algorithm", return false
+    "A man, a plan, a canal: Panama.", return true
+"""
+
+
+import re
+
+def valid_palindrome(str):
+    new_string = re.findall('[aA-zZ]', str)
+    output = ""
+    for i in new_string:
+        output += i
+    return output.lower() == output[::-1].lower()
+
+
+print(valid_palindrome("level"))
+print(valid_palindrome("algorithm"))
+print(valid_palindrome("A man, a plan, a canal: Panama."))
